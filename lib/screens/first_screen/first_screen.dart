91acc735e2a8cf682b3a4e299c92c98cf81090c4
Dilmurod <93697695+KhamdamovDilmurod@users.noreply.github.com>
2023-12-26 09:36:26 +0500

    Initial commit

---
+++ lib/screens/first_screen/first_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+
+    return FirstScreenState();
+  }
+  
+}
+class FirstScreenState extends State<FirstScreen>{
+  @override
+  Widget build(BuildContext context) {
+
+    return Container();
+  }
+  
+}
